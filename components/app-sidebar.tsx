aa5c6e8d0cb5e8837a41c3fa8158ac75110bbf39
Collin Caprini <caprinicollin@gmail.com>
2026-03-13 22:24:35 -0400

    updated dark mode toggle

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -6,17 +6,22 @@ import {
   SidebarHeader,
 } from "@/components/ui/sidebar"
 import { ModeToggle } from "./mode-toggle"
- 
+
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>
+        <h1 className="text-lg font-semibold">Luthene</h1>
+        <ModeToggle />
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter>
-        <ModeToggle />
+        <p className="text-sm text-muted-foreground">
+          © 2026 Two Daimons Software
+        </p>
       </SidebarFooter>
     </Sidebar>
   )
--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -5,37 +5,22 @@ import * as React from "react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { MoonIcon, SunIcon } from "@phosphor-icons/react"
+import { HeadlightsIcon } from "@phosphor-icons/react"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme, theme } = useTheme()
+
+  function toggleTheme() {
+    if (theme === "light") {
+      setTheme("dark")
+    } else {
+      setTheme("light")
+    }
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      <HeadlightsIcon className="h-[1.2rem] w-[1.2rem]" />
+    </Button>
   )
 }
